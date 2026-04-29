53967ff45f07396ee098c32a0517df4a70160c56
khs <hssmirt@gmail.com>
2026-04-29 11:23:17 +0900

    스크립트 로딩 방식 개선으로 showPopup 오류 수정

--- js/loader.js
+++ js/loader.js
@@ -11,28 +11,12 @@ const PARTIALS = [
   'place-search',
 ];
 
-function loadScript(src) {
-  return new Promise((resolve) => {
-    const s = document.createElement('script');
-    s.src = src;
-    s.onload = resolve;
-    document.body.appendChild(s);
-  });
-}
-
-async function loadPartials() {
+async function init() {
   const app = document.getElementById('app');
   const htmls = await Promise.all(
     PARTIALS.map(name => fetch(`partials/${name}.html`).then(r => r.text()))
   );
   htmls.forEach(html => app.insertAdjacentHTML('beforeend', html));
-}
-
-async function init() {
-  await loadPartials();
-  await loadScript('js/common.js');
-  await loadScript('js/main.js');
-  await loadScript('js/components.js');
   tryInit();
 }
 
